54bc8b993235ed4bbf41d5c690498fb6820c75dd
kiny010 <brianlau@Brians-iMac.local>
2017-04-30 21:04:48 -0700

    update

--- test/test.js
+++ test/test.js
@@ -2,17 +2,18 @@ import assert from "assert"
 import fs from "fs"
 import base64url from "./../lib/index"
 
-describe('jwt library', () => {
+describe('base64url', () => {
 
     describe('encode', () => {
-        it('should return endcoded string', () => {
+        it('should return endcoded string: aGVsbG8gd29ybGQ', () => {
             assert.equal("aGVsbG8gd29ybGQ", base64url.encode('hello world'))
         })
     })
 
     describe('decode', () => {
-        it('should return decoded string', () => {
+        it('should return decoded string: hello world', () => {
             assert.equal("hello world", base64url.decode('aGVsbG8gd29ybGQ'))
         })
     })
+
 })
